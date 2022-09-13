fd0338ab9788133caa7983da159c77a7d3d8d2b6
Terje <terjeslaatto@gmail.com>
2022-09-13 19:03:13 +0200

    cleanup. Can i commit and push at the same time? 😱

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,22 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            
-                    Text("Hello, world!")
-                        
-                    
-                    
-                    //RoundedRectange trenger en label, Text gjør ikke fordi det er en "selvfølge"
-                    //Shapes kan ha .stroke
-                    RoundedRectangle(cornerRadius: 50)
-                        .stroke(lineWidth: 3)
-                        
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-                }
-        ).padding(.horizontal).foregroundColor(.red)
-        
-
+        ZStack {
+            //RoundedRectange trenger en label, Text gjør ikke fordi det er en "selvfølge"
+            //Shapes kan ha .stroke
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(lineWidth: 3)
+            Text("Hello, world!")
+                }.padding(.horizontal).foregroundColor(.red)
 }
 }
 
