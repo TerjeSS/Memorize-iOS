4006a6d1fce7821fb261405a2768b8cf0e48acb0
Terje <terjeslaatto@gmail.com>
2022-09-13 18:56:28 +0200

    first commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        return ZStack(content: {
+            
+                    Text("Hello, world!")
+                        
+                    
+                    
+                    //RoundedRectange trenger en label, Text gjør ikke fordi det er en "selvfølge"
+                    //Shapes kan ha .stroke
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(lineWidth: 3)
+                        
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                }
+        ).padding(.horizontal).foregroundColor(.red)
+        
+
 }
+}
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
